c9cad2068ad170583391b8a351b5faf0238e6a34
Dam <damsauvagere@gmail.com>
2022-05-09 15:09:19 +0200

    feat: index w/ firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKhHZtcQ48115Ije3vS2l02xbMe7xcryY",
+  authDomain: "pwa-notif-damien.firebaseapp.com",
+  projectId: "pwa-notif-damien",
+  storageBucket: "pwa-notif-damien.appspot.com",
+  messagingSenderId: "707545206120",
+  appId: "1:707545206120:web:8cacb02758efa163bc9add",
+  measurementId: "G-7Q9JZSB5Q1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
